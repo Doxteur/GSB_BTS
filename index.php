e3279e2283d7776650e11fa57068b16bc06b04bb
PierreWZK <pierredolivet@live.fr>
2021-03-22 14:00:16 +0100

    Ajout branch pierre/index

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Design GSB</title>
+</head>
+<body>  
+
+    <div id="header">
+        <ul>
+            <li><a id="Login" href="#">Login</a></li>
+            <li><a href="#">Médicament</a></li>
+            <li><a href="#">Practiciens</a></li>
+            <li><a href="#">Visiteurs</a></li>
+            <li><a href="#">Comptes-Rendus</a></li>
+           </ul>
+    </div>
+
+
+
+</body>
+</html>
