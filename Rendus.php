4c068fa3ee1b8339387ab9e8ef531551a4a07d20
Doxteur <Doxteurn@gmail.com>
2021-04-12 20:12:33 +0200

    Design rapport presque finie

--- Rendus.php
+++ Rendus.php
@@ -11,19 +11,21 @@
 
 <body>
     <div id="header">
+
         <img src="Images/logo_2.png" alt="" id="logo">
         <div id="compteHeader">
             <img src="Images/Icons/avatar.png" alt="">
             <h1>Jhon Doe</h1>
         </div>
     </div>
+
     <div id="nav">
         <div id="inner_nav">
             <h2>Informations</h2>
             <ul>
                 <div>
                     <img src="Images/Icons/rapport.png" alt="Comptes-Rendus">
-                    <li>Comptes-Rendus</li>
+                    <li class="selected">Comptes-Rendus</li>
                 </div>
                 <div>
                     <img src="Images/Icons/doctor.png" alt="Praticien">
@@ -52,77 +54,86 @@
 
 
     <div id="content">
+        <div id="inner_content">
+            <?php
 
-        <?php
+            include("bddLogin.php");
 
-    include("bddLogin.php");
+            $reponse = $bdd->query('SELECT * FROM rapport_visite');
 
-    $reponse = $bdd->query('SELECT * FROM rapport_visite');
+            if (isset($_POST["numero_rapport"])) {
+                $numero_Rapport = $_POST["numero_rapport"];
+                $numeroRapport = $bdd->query('SELECT * FROM praticien INNER JOIN rapport_visite ON praticien.PRA_NUM = rapport_visite.PRA_NUM WHERE rapport_visite.RAP_NUM =' . $numero_Rapport . ';');
 
-    if (isset($_POST["numero_rapport"])) {
-        $numero_Rapport = $_POST["numero_rapport"];
-        $numeroRapport = $bdd->query('SELECT * FROM praticien INNER JOIN rapport_visite ON praticien.PRA_NUM = rapport_visite.PRA_NUM WHERE rapport_visite.RAP_NUM =' . $numero_Rapport . ';');
-
-        while ($data = $numeroRapport->fetch()) {
-            $valeurDate = $data["RAP_DATE"];
-            $valeurBilan = $data["RAP_BILAN"];
-            $praNum = $data["PRA_NUM"];
-            $rapMotif = $data["RAP_MOTIF"];
-            $nomPracticien = $data["PRA_NOM"];
-        }
-    } else {
-        $valeurDate = "";
-        $valeurBilan = " ";
-        $praNum = " ";
-        $rapMotif = " ";
-        $nomPracticien = " ";
-    }
-    ?>
+                while ($data = $numeroRapport->fetch()) {
+                    $valeurDate = $data["RAP_DATE"];
+                    $valeurBilan = $data["RAP_BILAN"];
+                    $praNum = $data["PRA_NUM"];
+                    $rapMotif = $data["RAP_MOTIF"];
+                    $nomPracticien = $data["PRA_NOM"];
+                    $prenomPraticien = $data["PRA_PRENOM"];
+                    
+                }
+            } else {
+                $valeurDate = "";
+                $valeurBilan = " ";
+                $praNum = " ";
+                $rapMotif = " ";
+                $nomPracticien = " ";
+                $prenomPraticien = " ";
+            }
+            ?>
 
             <form action="Rendus.php" method="post">
-                <label for="numero_rapport">Numero Rapport</label>
-                <select name="numero_rapport" id="searchRapport">
-            <?php
-             if (isset($_POST["numero_rapport"])) {
-                echo "<option selected='selected' disabled hidden>" . $_POST["numero_rapport"] . "</option>";
+                <label for="numero_rapport" id="numeroRapport">Numero Rapport : </label>
+                <div id="divRapport">
 
-            }
-                while ($data = $reponse->fetch()) {
-                    echo "<option>" . $data["RAP_NUM"] . "</option>";
-                }
-           
-            ?>
-        </select>
+                    <select name="numero_rapport" id="searchRapport">
+                        <?php
+                        if (isset($_POST["numero_rapport"])) {
+                            echo "<option selected='selected' disabled hidden>" . $_POST["numero_rapport"] . "</option>";
+                        }
+                        while ($data = $reponse->fetch()) {
+                            echo "<option>" . $data["RAP_NUM"] . "</option>";
+                        }
 
-                <input type="submit" value="chercher">
+                        ?>
+                    </select>
+
+                    <input type="submit" value="Chercher" id="search">
+                </div>
+                <hr>
                 <!-- Date -->
 
-                <label for="date_rapport">Date : </label>
+                <label for="date_rapport" id="dateLabel">Date : </label>
                 <?php
 
-        echo "<input type='text' name='date_rapport' value='" . strval($valeurDate) . "'>";
-        ?>
-                    <!-- Practicien -->
-                    <label for="practicien">Practicien :</label>
-                    <?php echo "<input type='text' name='praticien' value='" . strval($nomPracticien) . "'>";
-        ?>
+                echo "<input type='text' name='date_rapport' id='dateInput' value='" . strval($valeurDate) . "'>";
+                ?>
+                <!-- Practicien -->
+                <label for="practicien" id="labelPraticien">Practicien :</label>
+                <?php echo "<input type='text' name='praticien' id='inputPraticien'value='" . strval($nomPracticien) ." " .strval($prenomPraticien) . "'>";
+                ?>
 
 
-                    <!-- Motif -->
-                    <label for="motif">Motif Visite : </label>
-                    <?php
-        echo "<input type='text' name='motif' value='" . strval($rapMotif) . "'>";
-        ?>
-                        <!-- Bilan -->
-                        <label for="bilan">Bilan : </label>
-                        <?php
-        echo "<textarea type='text' name='bilan'>" . $valeurBilan  . "</textarea>";
-        ?>
-                            <!-- Offre d'échantillion -->
-                            <label for="offreEchan">Offre echantillion : </label>
-                            <input type="text" name="offreEchan">
+                <!-- Motif -->
+                <label for="motif" id="labelMotif">Motif Visite : </label>
+                <?php
+                echo "<input type='text' name='motif' id='inputMotif' value='" . strval($rapMotif) . "'>";
+                ?>
+                <!-- Bilan -->
+                <div id="bilan">
+                <label for="bilan" id="labelBilan">Bilan : </label>
+                <?php
+                echo "<textarea type='text' id='inputBilan' name='bilan'>" . $valeurBilan  . "</textarea>";
+                ?>
+            </div>
+                <!-- Offre d'échantillion -->
+                <label for="offreEchan">Offre echantillion : </label>
+                <input type="text" name="offreEchan">
 
             </form>
+        </div>
     </div>
 </body>
 
