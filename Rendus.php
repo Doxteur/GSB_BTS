51f259dd67541e3d3f1c85081da24194a14ddf43
Doxteur <Doxteurn@gmail.com>
2021-04-17 08:39:36 +0200

    Ajout d'une possibilité de creer des comptes rendus

--- Rendus.php
+++ Rendus.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,12 +17,14 @@
 </head>
 
 <body>
-    <div id="header">
 
-        <img src="Images/logo_2.png" alt="" id="logo">
+    <div id="header">
+        <a href="index.php">
+            <img src="Images/logo_2.png" alt="" id="logo">
+        </a>
         <div id="compteHeader">
             <img src="Images/Icons/avatar.png" alt="">
-            <h1>Jhon Doe</h1>
+            <h1><?= $_SESSION["nom"] . " " . $_SESSION["prenom"] ?></h1>
         </div>
     </div>
 
@@ -46,7 +53,7 @@
             <ul id="monCompte">
                 <div>
                     <img src="Images/Icons/avatar.png" alt="Medicaments">
-                    <li>Jhon Doe</li>
+                    <li><?= $_SESSION["nom"] . " " . $_SESSION["prenom"] ?></li>
                 </div>
             </ul>
         </div>
@@ -58,12 +65,49 @@
     <div id="content">
         <div id="inner_content">
             <?php
-            $increment = 1;
+            include("bddLogin.php");
+
+            if (!$_SESSION['role'] == "visiteur") {
+                visiteurPage();
+            } else {
+                praticienPage();
+            }
+            ?>
+
+            <div id="showPraticien" style="display:none">
+            </div>
+        </div>
+
+        <script>
+            function showPraticien() {
+                document.getElementById("showPraticien").style.display = "initial";
+            }
+
+            function Suivant() {
+                // document.getElementById("searchRapport").selectedIndex += document.getElementById(document.getElementById("searchRapport").selectedIndex).id;
+                var x = document.getElementById("searchRapport").selectedIndex;
+                var y = document.getElementById("searchRapport").options;
+
+                for (let i = 1; i < y.length; i++) {
+                    console.log(y[i].value + " " + y[0].value);
+                    if (y[i].value == y[x].value) {
+                        let temp = i;
+                        temp += 1;
+                        document.getElementById("searchRapport").selectedIndex += temp;
+                        break;
+                    }
+                }
+                document.forms["formulaire"].submit();
+
+            }
+        </script>
+        <?php
+        function praticienPage()
+        {
             include("bddLogin.php");
 
             $reponse = $bdd->prepare('SELECT * FROM rapport_visite');
             $reponse->execute();
-
             if (isset($_POST["numero_rapport"])) {
                 $numero_Rapport = $_POST["numero_rapport"];
                 $numeroRapport = $bdd->prepare('SELECT * FROM praticien INNER JOIN rapport_visite ON praticien.PRA_NUM = rapport_visite.PRA_NUM WHERE rapport_visite.RAP_NUM =' . $numero_Rapport . ';');
@@ -87,20 +131,21 @@
                 $nomPracticien = " ";
                 $prenomPraticien = " ";
             }
-            ?>
+
+
+
+        ?>
 
             <form action="Rendus.php" method="post" id="formulaire">
                 <label for="numero_rapport" id="numeroRapport">Numero Rapport : </label>
                 <div id="divRapport">
-
                     <select name="numero_rapport" id="searchRapport">
                         <?php
                         if (isset($_POST["numero_rapport"])) {
-                            echo "<option selected='selected' disabled hidden value=". $_POST["numero_rapport"]." id=".$_POST["numero_rapport"].">" . $_POST["numero_rapport"] . "</option>";
+                            echo "<option selected='selected' disabled hidden value=" . $_POST["numero_rapport"] . " id=" . $_POST["numero_rapport"] . ">" . $_POST["numero_rapport"] . "</option>";
                         }
                         while ($data = $reponse->fetch()) {
-                            echo "<option value=".$data["RAP_NUM"]." id=".$increment.">" . $data["RAP_NUM"] . "</option>";
-                            $increment++;
+                            echo "<option value=" . $data["RAP_NUM"] . ">" . $data["RAP_NUM"] . "</option>";
                         }
 
                         ?>
@@ -157,50 +202,142 @@
                     ?>
 
                 </div>
-                <h1 onclick="Suivant() ">Suivant</h1>
-                <h1></h1>
-        </div>
-        </form>
+            </form>
+            <div id="myButton">
+                <h1 onclick="Suivant()" id="suivant">Suivant</h1>
+                <h1 onclick="Nouveau()" id="nouveau">Nouveau</h1>
+                <h1 onclick="MettreAJour()" id="Update">Envoyer</h1>
+            </div>
 
     </div>
 
-    <div id="showPraticien" style="display:none">
 
+<?php }
+        function visiteurPage()
+        {
+            include("bddLogin.php");
+            $reponse = $bdd->prepare('SELECT * FROM praticien INNER JOIN rapport_visite ON praticien.PRA_NUM = rapport_visite.PRA_NUM WHERE VIS_MATRICULE = "' . $_SESSION["matricule"] . '";');
+            $reponse->execute();
 
-    </div>
-
-
-
-    </div>
-</body>
-
-<script>
-    function showPraticien() {
-        document.getElementById("showPraticien").style.display = "initial";
-    }
-    function Suivant() {
-        // document.getElementById("searchRapport").selectedIndex += document.getElementById(document.getElementById("searchRapport").selectedIndex).id;
-        var x = document.getElementById("searchRapport").selectedIndex;
-        var y = document.getElementById("searchRapport").options;
-
-        for(let i = 1; i < y.length;i++){
-            console.log(y[i].value + " " + y[0].value);
-            if(y[i].value == y[x].value){
-                let temp = i;
-                temp+=1;
-                document.getElementById("searchRapport").selectedIndex += temp;
-                break;
+            if (!isset($_POST["numero_rapport"])) {
+                $visiteurRapports = $bdd->prepare('SELECT * FROM praticien INNER JOIN rapport_visite ON praticien.PRA_NUM = rapport_visite.PRA_NUM WHERE VIS_MATRICULE = "' . $_SESSION["matricule"] . '";');
+                $visiteurRapports->execute();
+            } else {
+                $visiteurRapports = $bdd->prepare('SELECT * FROM praticien INNER JOIN rapport_visite ON praticien.PRA_NUM = rapport_visite.PRA_NUM WHERE RAP_NUM = "' . $_POST["numero_rapport"] . '";');
+                $visiteurRapports->execute();
             }
+
+            $medicament = $bdd->prepare('SELECT * FROM offrir INNER JOIN medicament ON offrir.MED_DEPOTLEGAL = medicament.MED_DEPOTLEGAL WHERE offrir.VIS_MATRICULE ="' . $_SESSION["matricule"] . '";');
+            $medicament->execute();
+
+            while ($data = $visiteurRapports->fetch()) {
+                $valeurDate = $data["RAP_DATE"];
+                $valeurBilan = $data["RAP_BILAN"];
+                $praNum = $data["PRA_NUM"];
+                $rapMotif = $data["RAP_MOTIF"];
+                $nomPracticien = $data["PRA_NOM"];
+                $prenomPraticien = $data["PRA_PRENOM"];
+            }
+?>
+
+    <form action="Rendus.php" method="post" id="formulaire">
+        <label for="numero_rapport" id="numeroRapport">Numero Rapport : </label>
+        <div id="divRapport">
+            <select name="numero_rapport" id="searchRapport">
+                <?php
+                $visiteurRapports->execute();
+                if (isset($_POST["numero_rapport"])) {
+                    echo "<option selected='selected' disabled hidden value=" . $_POST["numero_rapport"] . " id=" . $_POST["numero_rapport"] . ">" . $_POST["numero_rapport"] . "</option>";
+                }
+                while ($data = $reponse->fetch()) {
+                    echo "<option value=" . $data["RAP_NUM"] . " id=" . $increment . ">" . $data["RAP_NUM"] . "</option>";
+                }
+
+                ?>
+            </select>
+
+            <input type="submit" value="Chercher" id="search">
+        </div>
+        <hr>
+        <!-- Date -->
+
+        <label for="date_rapport" id="dateLabel">Date : </label>
+        <?php
+
+            echo "<input type='text' name='date_rapport' id='dateInput' value='" . strval($valeurDate) . "'>";
+        ?>
+        <!-- Practicien -->
+
+        <label for="practicien" id="labelPraticien">Practicien :</label>
+        <div>
+            <?php echo "<input type='text' name='praticien' id='inputPraticien'value='" . strval($nomPracticien) . " " . strval($prenomPraticien) . "'>";
+            ?>
+            <input type="button" value="En Details" id="search" onclick="showPraticien()">
+        </div>
+
+        <!-- Motif -->
+        <label for="motif" id="labelMotif">Motif Visite : </label>
+        <?php
+            echo "<input type='text' name='motif' id='inputMotif' value='" . strval($rapMotif) . "'>";
+        ?>
+        <!-- Bilan -->
+        <div id="bilan">
+            <label for="bilan" id="labelBilan">Bilan : </label>
+            <?php
+            echo "<textarea type='text' id='inputBilan' name='bilan'>" . $valeurBilan  . "</textarea>";
+            ?>
+        </div>
+
+
+        <!-- Offre d'échantillion -->
+        <label for="offreEchan" id="offre">Offre echantillion : </label>
+        <div id="medicament">
+            <?php
+
+            while ($data = $medicament->fetch()) {
+                $NomMedic = $data["MED_NOMCOMMERCIAL"];
+                $nombre = $data["OFF_QTE"];
+                echo "<div id='" . $NomMedic . "'>";
+                echo "<h1> " . $NomMedic . " : </h1> </br>";
+                echo "<h1> " . $nombre . "</h1>";
+                echo "</div>";
+            }
+            ?>
+
+        </div>
+    </form>
+    <div>
+        <h1 onclick="Suivant()" id="suivant">Suivant</h1>
+        <h1 onclick="Nouveau()" id="nouveau">Nouveau</h1>
+    </div>
+<?php
         }
-        document.forms["formulaire"].submit();
+?>
+<script>
+    function Nouveau() {
+        document.getElementById("Update").style.visibility = "visible"
+        document.getElementById("dateInput").value = " ";
+        document.getElementById("inputPraticien").value = " ";
+        document.getElementById("inputMotif").value = " ";
+        document.getElementById("inputBilan").value = " ";
+        document.getElementById("inputBilan").value = " ";
+        document.getElementById("inputBilan").value = " ";
+        var x = document.getElementById("searchRapport");
+        var option = document.createElement("option");
+        var lastValue = $('#searchRapport option:last-child').val();
+        lastValue = 1 + +lastValue
+        option.text = lastValue;
+        option.value = lastValue;
+        console.log(typeof(lastValue))
+        x.add(option);
+        $('#searchRapport').val($('#searchRapport option:last-child').val());
 
     }
 
-    function Precedent() {
+    function MettreAjour() {
 
     }
 </script>
-
-
+</body>
 
 </html>
